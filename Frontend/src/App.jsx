de8a387a4020c0d30087d49036d40b9e7aba5451
Jagdish Sahu <righteousknight007@gmail.com>
2025-03-16 18:18:42 +0530

    Resolved merge conflicts

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home"
@@ -24,5 +25,24 @@ const App = () => {
     </DoctorProvider>
   )
 }
+=======
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AllDoc from "./pages/AllDoc";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
-export default App
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/AllDoc" element={<AllDoc />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Contact" element={<Contact />} />
+    </Routes>
+  );
+};
+>>>>>>> 0ebb8b5 (backend server)
+
+export default App;
