632018832cb01fce2ef85f0d5c9d89a3da9ccd5f
SurjeetMandal <surjeetmandal698@gmail.com>
2025-03-17 11:21:38 +0530

    Some New Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home"
@@ -27,24 +26,5 @@ const App = () => {
     </DoctorProvider>
   )
 }
-=======
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import AllDoc from "./pages/AllDoc";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/AllDoc" element={<AllDoc />} />
-      <Route path="/About" element={<About />} />
-      <Route path="/Contact" element={<Contact />} />
-    </Routes>
-  );
-};
->>>>>>> 0ebb8b5 (backend server)
 
 export default App;
